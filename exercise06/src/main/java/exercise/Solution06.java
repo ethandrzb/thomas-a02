0c4b93a5e6a1069f366b54ef7e585673069a4009
ethandrzb <ethandrzb@gmail.com>
2021-09-16 19:57:01 -0400

    Made Scanner private

--- exercise06/src/main/java/exercise/Solution06.java
+++ exercise06/src/main/java/exercise/Solution06.java
@@ -10,8 +10,10 @@ import java.util.Scanner;
 
 public class Solution06
 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
         int currentAge;
         int retireAge;
 
