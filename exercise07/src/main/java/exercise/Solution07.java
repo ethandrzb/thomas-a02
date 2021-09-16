4e0737da1c261a1eea39aee54cf9526d1ffbd666
ethandrzb <ethandrzb@gmail.com>
2021-09-16 11:45:32 -0400

    Start of Exercise 07

---
+++ exercise07/src/main/java/exercise/Solution07.java
@@ -0,0 +1,14 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 6
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package exercise;
+
+public class Solution07
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Some text");
+    }
+}
