75c189480f763fe4233c456e3c036138b5322452
ethandrzb <ethandrzb@gmail.com>
2021-09-16 11:36:06 -0400

    Revert "Split solution into functions"
    This reverts commit c01c0ce0

--- exercise06/src/main/java/exercise/Solution06.java
+++ exercise06/src/main/java/exercise/Solution06.java
@@ -6,13 +6,16 @@
 package exercise;
 
 import java.util.Scanner;
+import java.util.Calendar;
 
 public class Solution06
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         int currentAge;
         int retireAge;
+        int currentYear;
 
         System.out.print("What is your current age? ");
 
@@ -22,8 +25,11 @@ public class Solution06
 
         retireAge = sc.nextInt();
 
-        Retirement r = new Retirement(currentAge, retireAge);
-        r.printRetirement();
+        System.out.printf("You have %d years left until you can retire.%n", (retireAge - currentAge));
+
+        currentYear = Calendar.getInstance().get(Calendar.YEAR);
+
+        System.out.printf("It's %d, so you can retire in %d.%n", currentYear, currentYear + (retireAge - currentAge));
 
         System.exit(0);
     }
