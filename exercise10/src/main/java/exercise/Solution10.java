e5d43a260634b172fc36156e60c8e78ad0eb58ab
ethandrzb <ethandrzb@gmail.com>
2021-09-16 23:12:57 -0400

    Start of Solution10

---
+++ exercise10/src/main/java/exercise/Solution10.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 10
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package exercise;
+
+public class Solution10
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Here's some text.");
+        System.out.println("Enjoy!");
+
+        System.exit(0);
+    }
+}
