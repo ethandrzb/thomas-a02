88071d4fc726f7a61d68355ccda3610882286f01
ethandrzb <ethandrzb@gmail.com>
2021-09-17 19:34:33 -0400

    Finished Solution15

--- exercise15/src/main/java/exercise/Solution15.java
+++ exercise15/src/main/java/exercise/Solution15.java
@@ -5,12 +5,38 @@
 
 package exercise;
 
+import java.util.Scanner;
+
 public class Solution15
 {
+    private static final Scanner sc = new Scanner(System.in);
+
+    private static final String PASSWORD = "abc$123";
+
     public static void main(String[] args)
     {
-        System.out.println("Some text to bring you joy.");
+        Solution15 sol = new Solution15();
+
+        System.out.print("What is the password? ");
+        String inputPassword = sc.nextLine();
+
+        sol.printResponse(inputPassword);
 
         System.exit(0);
+    }
+    private boolean checkPassword(String inputPassword)
+    {
+        return inputPassword.equals(PASSWORD);
+    }
+    private void printResponse(String inputPassword)
+    {
+        if(checkPassword(inputPassword))
+        {
+            System.out.println("Welcome!");
+        }
+        else
+        {
+            System.out.println("I don't know you.");
+        }
     }
 }
