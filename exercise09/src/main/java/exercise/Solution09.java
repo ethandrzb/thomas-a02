199690cbd0ed1fbb863b6c2d46d50aa032fc12f4
ethandrzb <ethandrzb@gmail.com>
2021-09-16 21:45:39 -0400

    Finished Solution09.java

--- exercise09/src/main/java/exercise/Solution09.java
+++ exercise09/src/main/java/exercise/Solution09.java
@@ -5,12 +5,40 @@
 
 package exercise;
 
+import java.util.Scanner;
+
 public class Solution09
 {
+    private static final Scanner sc = new Scanner(System.in);
+
+    private static final int SQ_FEET_PER_CAN = 350;
+    private double length;
+    private double width;
+
     public static void main(String[] args)
     {
-        System.out.println("Some text");
+        Solution09 sol = new Solution09();
+
+        sol.getDimensionsFromUser();
+        sol.getCeilingArea();
+
+        int numPaintCansNeeded = (int) Math.ceil(sol.getCeilingArea() / SQ_FEET_PER_CAN);
+
+        System.out.printf("You will need to purchase %d gallons of paint to cover %.3f square feet.%n",
+                numPaintCansNeeded, sol.getCeilingArea());
 
         System.exit(0);
+    }
+    public void getDimensionsFromUser()
+    {
+        System.out.println("What is the length of the ceiling?");
+        length = sc.nextDouble();
+
+        System.out.println("What is the width of the ceiling?");
+        width = sc.nextDouble();
+    }
+    public double getCeilingArea()
+    {
+        return length * width;
     }
 }
