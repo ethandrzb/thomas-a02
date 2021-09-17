6a6fc41def0c26cc05ccd838eabe5d695172e048
ethandrzb <ethandrzb@gmail.com>
2021-09-16 23:33:10 -0400

    Finished Solution10

--- exercise10/src/main/java/exercise/Solution10.java
+++ exercise10/src/main/java/exercise/Solution10.java
@@ -5,13 +5,61 @@
 
 package exercise;
 
+import java.util.Scanner;
+
 public class Solution10
 {
+    private static final Scanner sc = new Scanner(System.in);
+
+    private static final double SALES_TAX_RATE = 0.055;
+
+    private final double[] price = new double[3];
+    private final double[] quantity = new double[3];
+
     public static void main(String[] args)
     {
-        System.out.println("Here's some text.");
-        System.out.println("Enjoy!");
+        Solution10 sol = new Solution10();
+
+        sol.getShoppingCartFromUser();
+
+        System.out.println(sol.selfCheckout());
 
         System.exit(0);
+    }
+    public void getShoppingCartFromUser()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            System.out.print("Enter the price of item " + (i + 1) + " : ");
+            price[i] = sc.nextDouble();
+
+            System.out.print("Enter the quantity of item " + (i + 1) + " : ");
+            quantity[i] = sc.nextDouble();
+        }
+    }
+    public double getSubtotal()
+    {
+        double subtotal = 0;
+
+        for(int i = 0; i < 3; i++)
+        {
+            subtotal += price[i] * quantity[i];
+        }
+
+        return subtotal;
+    }
+    public double getTaxAmount()
+    {
+        return getSubtotal() * SALES_TAX_RATE;
+    }
+    public double getTotal()
+    {
+        return getSubtotal() * (1.0 + SALES_TAX_RATE);
+    }
+    public String selfCheckout()
+    {
+        return String.format("Subtotal: $%.2f%n" +
+                "Tax: $%.2f%n" +
+                "Total: $%.2f", getSubtotal(), getTaxAmount(), getTotal());
     }
 }
