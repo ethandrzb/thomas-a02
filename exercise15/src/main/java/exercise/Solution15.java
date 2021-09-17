267cb97757a15435fd1d185e7d2be5a1667d6406
ethandrzb <ethandrzb@gmail.com>
2021-09-17 19:27:56 -0400

    Start of Solution15

---
+++ exercise15/src/main/java/exercise/Solution15.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 15
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package exercise;
+
+public class Solution15
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Some text to bring you joy.");
+
+        System.exit(0);
+    }
+}
