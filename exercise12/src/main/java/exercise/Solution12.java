72e0f0cb77d720f61b6646a205aaa5860c712325
ethandrzb <ethandrzb@gmail.com>
2021-09-17 17:59:16 -0400

    Start of Solution12

---
+++ exercise12/src/main/java/exercise/Solution12.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 12
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package exercise;
+
+public class Solution12
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Some text");
+
+        System.exit(0);
+    }
+}
