7535ed1152c88f6db0e526c0ec9f0412d14db592
ethandrzb <ethandrzb@gmail.com>
2021-09-17 11:52:48 -0400

    Added pseudocode solution

--- exercise08/src/main/java/exercise/Solution08.java
+++ exercise08/src/main/java/exercise/Solution08.java
@@ -19,8 +19,10 @@ public class Solution08
     {
         Solution08 sol = new Solution08();
 
+        // Get pizza party information from user
         sol.getAllValuesFromUser();
 
+        // Display calculated pizza party attributes
         System.out.printf("%d people with %d pizzas (%d slices)%n", sol.numPeople, sol.numPizzas, sol.calculateTotalSlices());
         System.out.printf("Each person gets %d slices%n", sol.slicesPerPerson());
         System.out.printf("There are %d leftover slices%n", sol.leftoverSlices());
@@ -30,24 +32,35 @@ public class Solution08
 
     public void getAllValuesFromUser()
     {
+        // Prompt user for number of party attendees
         numPeople = getIntFromUser("How many people?");
+
+        // Prompt user for number of pizzas ordered
         numPizzas = getIntFromUser("How many pizzas do you have?");
+
+        // Prompt user for number of slices per pizza
         slicesPerPizza = getIntFromUser("How many slices per pizza?");
     }
 
     private int getIntFromUser(String prompt)
     {
+        // Display prompt
         System.out.println(prompt);
         return sc.nextInt();
     }
+    // Calculates total slices of pizza ordered
     private int calculateTotalSlices()
     {
         return numPizzas * slicesPerPizza;
     }
+
+    // Calculates number of pizza slices each person can have, making sure that everyone gets the same number of slices
     private int slicesPerPerson()
     {
         return calculateTotalSlices() / numPeople;
     }
+
+    // Calculates number of remaining pizza slices
     private int leftoverSlices()
     {
         return calculateTotalSlices() % numPeople;
