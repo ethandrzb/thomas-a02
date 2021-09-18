d88cc9679532856b4b843a5bb070bd81955ee5d4
ethandrzb <ethandrzb@gmail.com>
2021-09-18 18:20:33 -0400

    Start of Solution17

---
+++ exercise17/src/main/java/exercise/Solution17.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 17
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package exercise;
+
+public class Solution17
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Some text to make you feel nice and warm inside.");
+
+        System.exit(0);
+    }
+}
