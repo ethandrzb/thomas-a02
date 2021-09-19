38015c4168e150d82b6904e9d053674e74d39352
ethandrzb <ethandrzb@gmail.com>
2021-09-19 15:07:03 -0400

    Start of Solution22

---
+++ exercise22/src/main/java/exercise/Solution22.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 22
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package exercise;
+
+public class Solution22
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Some text to make you question your purpose in life.");
+
+        System.exit(0);
+    }
+}
