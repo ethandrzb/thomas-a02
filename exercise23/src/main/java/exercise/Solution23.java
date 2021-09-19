00ab79233fbfecc056312d99329e947f7456b74b
ethandrzb <ethandrzb@gmail.com>
2021-09-19 15:37:35 -0400

    Start of Solution23

---
+++ exercise23/src/main/java/exercise/Solution23.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 23
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package exercise;
+
+public class Solution23
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Some text to mark the last problem in this assignment.");
+
+        System.exit(0);
+    }
+}
