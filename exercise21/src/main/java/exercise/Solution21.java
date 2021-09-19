c3c7f4d1499bb73f48cf45d6c64ab54a37d46f3b
ethandrzb <ethandrzb@gmail.com>
2021-09-19 14:38:19 -0400

    Start of Solution21

---
+++ exercise21/src/main/java/exercise/Solution21.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 21
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package exercise;
+
+public class Solution21
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Some text to give me the courage to continue my work.");
+        System.out.println("I hope it encourages you to do the same.");
+
+        System.exit(0);
+    }
+}
