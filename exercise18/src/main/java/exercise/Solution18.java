5ae08535e821f0013f4ee2ee5f6da9d9a03dc0fc
ethandrzb <ethandrzb@gmail.com>
2021-09-19 10:36:52 -0400

    Start of Solution18

---
+++ exercise18/src/main/java/exercise/Solution18.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 18
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package exercise;
+
+public class Solution18
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Some text to conjure the build folder from the ashes");
+
+        System.exit(0);
+    }
+}
