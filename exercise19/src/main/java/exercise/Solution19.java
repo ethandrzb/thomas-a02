40c26092b7e7ee013556c4b9a4df1ca0d272c7df
ethandrzb <ethandrzb@gmail.com>
2021-09-19 11:24:56 -0400

    Start of Solution19

---
+++ exercise19/src/main/java/exercise/Solution19.java
@@ -0,0 +1,16 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution 19
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package exercise;
+
+public class Solution19
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Some text to keep your mind centered in these times of strife.");
+
+        System.exit(0);
+    }
+}
