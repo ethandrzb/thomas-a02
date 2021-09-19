86d7baff915bb17db8556da57b6c3699f62ade7e
ethandrzb <ethandrzb@gmail.com>
2021-09-19 12:45:22 -0400

    Finished Solution20

--- exercise20/src/main/java/exercise/Solution20.java
+++ exercise20/src/main/java/exercise/Solution20.java
@@ -5,12 +5,80 @@
 
 package exercise;
 
+import java.util.Scanner;
+
 public class Solution20
 {
+    private static final Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args)
     {
-        System.out.println("Some text to make you wonder how long I'm going to keep doing this.");
+        Solution20 sol = new Solution20();
+
+        // Prompt user for order amount
+        double orderAmount = Double.parseDouble(sol.getStringFromUser("What is the order amount?"));
+
+        // Prompt user for state
+        String userState = sol.getStringFromUser("What state do you live in?");
+
+        // Prompt user for county
+        String userCounty = sol.getStringFromUser("What county do you live in?");
+
+        // Calculate sales tax
+        double salesTax = sol.getSalesTax(orderAmount, userState, userCounty);
+
+        // Calculate total
+        double total = orderAmount + salesTax;
+
+        // Display sales tax and total
+        System.out.printf("The tax is $%.2f.%n" +
+                "The total is $%.2f.%n", salesTax, total);
 
         System.exit(0);
+    }
+
+    private String getStringFromUser(String prompt)
+    {
+        System.out.print(prompt + " ");
+
+        return sc.nextLine();
+    }
+
+    private double getSalesTax(double subtotal, String state, String county)
+    {
+        double taxMultiplier = 0.0;
+        double taxAmount;
+
+        if(state.equals("Wisconsin"))
+        {
+            taxMultiplier = 0.05;
+            if(county.equals("Eau Claire"))
+            {
+                taxMultiplier += 0.005;
+            }
+            else if(county.equals("Dunn"))
+            {
+                taxMultiplier += 0.004;
+            }
+        }
+        else if(state.equals("Illinois"))
+        {
+            taxMultiplier = 0.08;
+        }
+
+        taxAmount = subtotal * taxMultiplier;
+
+        System.out.println("Tax amount before rounding: " + taxAmount);
+
+        // Round up to next penny
+        taxAmount *= 1000;
+        taxAmount = Math.ceil(taxAmount);
+        taxAmount /= 10;
+        taxAmount = Math.ceil(taxAmount);
+        taxAmount /= 100;
+
+        System.out.println("Tax amount after rounding: " + taxAmount);
+
+        return taxAmount;
     }
 }
